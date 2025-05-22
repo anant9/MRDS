91decd324e89d7946d71411ca10f3b0bb61580fe
anant9 <anantgoel09@gmail.com>
2025-05-22 08:35:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from agents.data_agent import DataAgent
 from agents.cleaner_agent import CleanerAgent
 from agents.analyzer_agent import AnalyzerAgent
